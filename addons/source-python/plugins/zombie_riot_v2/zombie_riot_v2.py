661f572d7cea1eab720b649b7eb1ce9ce6f2711b
srpg <44681214+srpg@users.noreply.github.com>
2025-01-30 14:53:14 +0200

    Fixed model not propertly set if have 0 multiple models path

--- addons/source-python/plugins/zombie_riot_v2/zombie_riot_v2.py
+++ addons/source-python/plugins/zombie_riot_v2/zombie_riot_v2.py
@@ -290,7 +290,7 @@ def get_zombie_model():
     models = []
     for i in _settings['zr'][f'{day}']['model']:
         models.append(i.split(','))
-    if len(models) is 1:
+    if len(models) is 0:
         return _settings['zr'][f'{day}']['model']
     else:
         return choice(models)
